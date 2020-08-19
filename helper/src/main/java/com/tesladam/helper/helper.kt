69a08756ea960eb49157632a2d1629d9c70baa20
Hakan Yılmaz <hkn1899@gmail.com>
2020-08-19 18:08:49 +0300

    Güvenlik sorgusu düzeltildi

--- helper/src/main/java/com/tesladam/helper/helper.kt
+++ helper/src/main/java/com/tesladam/helper/helper.kt
@@ -48,8 +48,7 @@ import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
-private var guvenlik: Boolean? = null
-private var ctx: Context? = null
+private var helper_guvenlik: helperGuvenlik? = null
 
 object helper : Application() {
     @JvmStatic
@@ -132,6 +131,7 @@ object helper : Application() {
 }
 
 class helperActivity(private val context: Context) : ContextWrapper(context) {
+
     fun degistir(ekran: Class<*>) {
         startActivity(Intent(this, ekran))
     }
@@ -176,9 +176,17 @@ class helperAdapterRecycler(
     }
 
     override fun getItemCount(): Int {
-        if (guvenlik == null) helperGuvenlik(ctx!!)
-        if (guvenlik!!){
-            return size
+        if (helper_guvenlik != null){
+            var bosMu = true
+
+            helper_guvenlik?.kontrol(object : Guvenlik{
+                override fun Kontrol(sonuc: Boolean) {
+                    bosMu = sonuc
+                }
+            })
+
+            if (bosMu)
+                return size
         }
         return 0
     }
@@ -192,8 +200,18 @@ class helperAdapterRecycler(
 object helperBellek {
     @JvmStatic
     fun getBellek(context: Context, bellekKey: String): Any? {
-        if (guvenlik == null) helperGuvenlik(context)
-        if (guvenlik!!){
+        if (helper_guvenlik == null)
+            helper_guvenlik = helperGuvenlik(context)
+
+        var bosMu = true
+
+        helper_guvenlik?.kontrol(object : Guvenlik{
+            override fun Kontrol(sonuc: Boolean) {
+                bosMu = sonuc
+            }
+        })
+
+        if (bosMu){
             return context.getSharedPreferences(bellekKey, Context.MODE_PRIVATE).all[bellekKey]
         }
         return null
@@ -201,8 +219,18 @@ object helperBellek {
 
     @JvmStatic
     fun setBellek(context: Context, bellekKey: String, bellekValue: String) {
-        if (guvenlik == null) helperGuvenlik(context)
-        if (guvenlik!!){
+        if (helper_guvenlik == null)
+            helper_guvenlik = helperGuvenlik(context)
+
+        var bosMu = true
+
+        helper_guvenlik?.kontrol(object : Guvenlik{
+            override fun Kontrol(sonuc: Boolean) {
+                bosMu = sonuc
+            }
+        })
+
+        if (bosMu){
             val shared = context.getSharedPreferences(bellekKey, Context.MODE_PRIVATE)
             shared.edit().putString(bellekKey, bellekValue).apply()
         }
@@ -285,8 +313,25 @@ object helperFragment {
 }
 
 class helperGuvenlik(private val context: Context) : ContextWrapper(context){
+
+    companion object{
+        private var mCtx: Context? = null
+        private var res = false
+    }
+
+    private var ctx1: Context? = null
+
+    val ctx2: Context?
+        get() {
+            if (ctx1 == null)
+                ctx1 = mCtx
+            return ctx1
+        }
+
     init {
-        ctx = context
+        mCtx = context
+
+/*        ctx = context
         val json = helperJson(applicationContext)
         json.singleton.requestQueue?.cache?.clear()
 
@@ -326,7 +371,52 @@ class helperGuvenlik(private val context: Context) : ContextWrapper(context){
 
             }
         }
+*/
+    }
 
+    fun kontrol(guvenlik: Guvenlik){
+
+        val json = helperJson(ctx2!!)
+        json.singleton.requestQueue?.cache?.clear()
+
+        if (!res && helperInternetErisimi(ctx2!!).internetErisimi()){
+            json.getSirali("https://tesladam.herokuapp.com/res/$packageName"){
+                val sonuc = JSONObject(it)
+
+                //Uygulama Güvenliği
+                res = sonuc.getInt("status") == 1
+                Log.d("asdasd", "Uygulama Güvenliği: $res")
+
+                //Version Kontrollü
+                if (res){
+                    if (sonuc.getInt("version_status") != -1 && sonuc.getInt("version_status") == 1 &&
+                        sonuc.getString("version") != "-1" && sonuc.getString("version") != ctx2!!.packageManager.getPackageInfo(packageName, 0).versionName){
+
+                        Log.d("asdasd", "Version Kontrollü: true")
+                        val diller = sonuc.getString("version_lang").split(",")
+                        var dil = ""
+
+                        if (diller.size == 1)
+                            dil = diller[0]
+
+                        val title = if(dil.equals("tr")) "Uyarı" else if (dil.equals("en")) "Warning" else "Предупреждение"
+
+                        val message = if(dil.equals("tr")) "Uygulamanın yeni versiyonu mevcut. Lütfen uygulamayı güncelleyin."
+                        else if (dil.equals("en")) "New version of the application is available. Please update the application."
+                        else "Доступна новая версия приложения. Пожалуйста, обновите приложение."
+
+                        val tamam = if(dil.equals("tr")) "Tamam" else if (dil.equals("en")) "Okay" else "Ладно"
+
+                        helperPopUp(this, title, message, tamam, {
+                            //startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$packageName")))
+                            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.veryansintv.app")))
+                        }).show()
+                    }
+                }
+            }
+        }
+
+        guvenlik.Kontrol(res)
     }
 }
 
@@ -343,22 +433,28 @@ class helperIzinAl(
     private val function: () -> Unit
 ) : ContextWrapper(context) {
     init {
-        if (guvenlik == null) helperGuvenlik(context)
-        if (guvenlik!!){
-            Dexter.withContext(context)
-                .withPermission(izin)
-                .withListener(object : PermissionListener {
-                    override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
-                        function.invoke()
-                    }
+        if (helper_guvenlik == null)
+            helper_guvenlik = helperGuvenlik(context)
 
-                    override fun onPermissionRationaleShouldBeShown(p0: PermissionRequest?, p1: PermissionToken?) {
-                        p1?.continuePermissionRequest()
-                    }
+        helper_guvenlik?.kontrol(object : Guvenlik{
+            override fun Kontrol(sonuc: Boolean) {
+                if (sonuc){
+                    Dexter.withContext(context)
+                        .withPermission(izin)
+                        .withListener(object : PermissionListener {
+                            override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                                function.invoke()
+                            }
 
-                    override fun onPermissionDenied(p0: PermissionDeniedResponse?) {}
-                }).check()
-        }
+                            override fun onPermissionRationaleShouldBeShown(p0: PermissionRequest?, p1: PermissionToken?) {
+                                p1?.continuePermissionRequest()
+                            }
+
+                            override fun onPermissionDenied(p0: PermissionDeniedResponse?) {}
+                        }).check()
+                }
+            }
+        })
     }
 }
 
@@ -368,21 +464,27 @@ class helperJson(private val context: Context) : ContextWrapper(context) {
     val tickle = VolleyTickle.newRequestTickle(context)
 
     fun get(url: String, tur: Int, result: (Any) -> Unit) {
-        if (guvenlik == null) helperGuvenlik(context)
-        if (guvenlik!!){
-            if (tur == helper.array) {
-                val arrayRequest = JsonArrayRequest(url, Response.Listener {
-                    result.invoke(it)
-                }, Response.ErrorListener { })
-                singleton.addToRequestQueue(arrayRequest)
+        if (helper_guvenlik == null)
+            helper_guvenlik = helperGuvenlik(context)
+
+        helper_guvenlik?.kontrol(object : Guvenlik{
+            override fun Kontrol(sonuc: Boolean) {
+                if (sonuc){
+                    if (tur == helper.array) {
+                        val arrayRequest = JsonArrayRequest(url, {
+                            result.invoke(it)
+                        }, { })
+                        singleton.addToRequestQueue(arrayRequest)
+                    }
+                    else {
+                        val objeRequest = JsonObjectRequest(url, null, {
+                            result.invoke(it)
+                        }, { })
+                        singleton.addToRequestQueue(objeRequest)
+                    }
+                }
             }
-            else {
-                val objeRequest = JsonObjectRequest(url, null, Response.Listener {
-                    result.invoke(it)
-                }, Response.ErrorListener { })
-                singleton.addToRequestQueue(objeRequest)
-            }
-        }
+        })
     }
 
     fun getSirali(url: String, result: (String) -> Unit) {
@@ -402,39 +504,51 @@ class helperJson(private val context: Context) : ContextWrapper(context) {
     fun getXml(url: String, result: (JSONObject) -> Unit) {
         StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
 
-        if (guvenlik == null) helperGuvenlik(context)
-        if (guvenlik!!){
-            val stringRequest = StringRequest(Request.Method.GET, url, null, null)
-            tickle.add(stringRequest)
-            val response = tickle.start()
+        if (helper_guvenlik == null)
+            helper_guvenlik = helperGuvenlik(context)
 
-            if (response.statusCode == 200) {
-                val data = VolleyTickle.parseResponse(response)
-                result.invoke(JSONObject(U.xmlToJson(data)))
-            } else
-                Log.d("asdasd", "Helper Json getXml Hata")
-        }
+        helper_guvenlik?.kontrol(object : Guvenlik{
+            override fun Kontrol(sonuc: Boolean) {
+                if (sonuc){
+                    val stringRequest = StringRequest(Request.Method.GET, url, null, null)
+                    tickle.add(stringRequest)
+                    val response = tickle.start()
+
+                    if (response.statusCode == 200) {
+                        val data = VolleyTickle.parseResponse(response)
+                        result.invoke(JSONObject(U.xmlToJson(data)))
+                    } else
+                        Log.d("asdasd", "Helper Json getXml Hata")
+                }
+            }
+        })
     }
 
     fun post( url: String, postKey: HashMap<String, String>,  result: (String) -> Unit ){
 
-        if (guvenlik == null) helperGuvenlik(context)
-        if (guvenlik!!){
-            val request = object : StringRequest(Method.POST, url, Response.Listener {
-                result.invoke(it)
-            }, Response.ErrorListener {
-                Log.d("asdasd", "post: $it")
-                result.invoke("gönderilemedi")
-            }){
-                override fun getParams(): MutableMap<String, String> {
-                    super.getParams()
-                    Log.d("asdasd", "getParams: $postKey")
-                    return postKey
+        if (helper_guvenlik == null)
+            helper_guvenlik = helperGuvenlik(context)
+
+        helper_guvenlik?.kontrol(object : Guvenlik{
+            override fun Kontrol(sonuc: Boolean) {
+                if (sonuc){
+                    val request = object : StringRequest(Method.POST, url, Response.Listener {
+                        result.invoke(it)
+                    }, Response.ErrorListener {
+                        Log.d("asdasd", "post: $it")
+                        result.invoke("gönderilemedi")
+                    }){
+                        override fun getParams(): MutableMap<String, String> {
+                            super.getParams()
+                            Log.d("asdasd", "getParams: $postKey")
+                            return postKey
+                        }
+                    }
+
+                    singleton.addToRequestQueue(request)
                 }
             }
-
-            singleton.addToRequestQueue(request)
-        }
+        })
     }
 }
 
@@ -443,6 +557,7 @@ class helperPopUp(private val context: Context, private val title: String = "", 
                   private val negative: String = "", private val negaClick: () -> Unit = {},
                   private val natural: String = "", private val natuClick: () -> Unit = {}): ContextWrapper(context){
     private val builder = AlertDialog.Builder(context)
+    private var bosMu = true
 
     init {
         if (mesaj.isNotEmpty())
@@ -461,25 +576,29 @@ class helperPopUp(private val context: Context, private val title: String = "", 
             builder.setNeutralButton(natural) { _, _ -> natuClick.invoke() }
 
         builder.create()
+
+        if (helper_guvenlik == null)
+            helper_guvenlik = helperGuvenlik(context)
+
+        helper_guvenlik?.kontrol(object : Guvenlik{
+            override fun Kontrol(sonuc: Boolean) {
+                bosMu = sonuc
+            }
+        })
     }
 
     fun show(): AlertDialog? {
-        if (guvenlik == null) helperGuvenlik(context)
-        if (guvenlik!!){
+        if (bosMu)
             return builder.show()!!
-        }
         return null
     }
     fun get(): AlertDialog? {
-        if (guvenlik == null) helperGuvenlik(context)
-        if (guvenlik!!){
+        if (bosMu)
             return builder.create()!!
-        }
         return null
     }
     fun view(view: View): helperPopUp? {
-        if (guvenlik == null) helperGuvenlik(context)
-        if (guvenlik!!){
+        if (bosMu){
             builder.setView(view)
             return this
         }
@@ -523,29 +642,41 @@ class helperResimYukle(
     val img: ImageView
 ) : ContextWrapper(context) {
     init {
-        if (guvenlik == null) helperGuvenlik(context)
-        if (guvenlik!!){
-            Glide.with(context)
-                .load(url)
-                .into(img)
-        }
+        if (helper_guvenlik == null)
+            helper_guvenlik = helperGuvenlik(context)
+
+        helper_guvenlik?.kontrol(object : Guvenlik{
+            override fun Kontrol(sonuc: Boolean) {
+                if (sonuc){
+                    Glide.with(context)
+                        .load(url)
+                        .into(img)
+                }
+            }
+        })
     }
 }
 
 class helperSlider(private val carouselView: CarouselView) {
+
+    private var bosMu = true
+
+    init {
+        if (helper_guvenlik == null)
+            bosMu = false
+    }
+
     fun slider(): CarouselView? {
-        if (guvenlik == null) helperGuvenlik(ctx!!)
-        if (guvenlik!!){
+        if (bosMu){
             carouselView.mode = CarouselView.Mode.SNAP
             carouselView.sideBySideStyle = CarouselView.PreviewSideBySideStyle.NORMAL
             return carouselView
         }
-        return null
+        return carouselView
     }
 
     fun carousel1(): CarouselView? {
-        if (guvenlik == null) helperGuvenlik(ctx!!)
-        if (guvenlik!!){
+        if (bosMu){
             carouselView.itemMargin = 10 * 3
             carouselView.mode = CarouselView.Mode.PREVIEW
             carouselView.previewOffset = 30 * 3
@@ -553,12 +684,11 @@ class helperSlider(private val carouselView: CarouselView) {
             carouselView.sideBySideStyle = CarouselView.PreviewSideBySideStyle.NORMAL
             return carouselView
         }
-        return null
+        return carouselView
     }
 
     fun carousel2(): CarouselView? {
-        if (guvenlik == null) helperGuvenlik(ctx!!)
-        if (guvenlik!!){
+        if (bosMu){
             carouselView.itemMargin = 10 * 3
             carouselView.mode = CarouselView.Mode.PREVIEW
             carouselView.previewOffset = 30 * 3
@@ -567,7 +697,7 @@ class helperSlider(private val carouselView: CarouselView) {
             carouselView.sideBySideStyle = CarouselView.PreviewSideBySideStyle.SCALE
             return carouselView
         }
-        return null
+        return carouselView
     }
 }
 
@@ -606,4 +736,8 @@ interface dinle {
 
 interface bildirim{
     fun bildirim(geldi: Boolean, data: String)
+}
+
+interface Guvenlik{
+    fun Kontrol(sonuc: Boolean)
 }
